6fbb988e2537477561810c2bbe945f8d6f8841ac
Rafael Gómez Bermejo <sernn2@gmail.com>
2019-06-24 16:51:34 +0200

    limpieza + añadido fichero de dependencias

--- R_big_data/tools.py
+++
@@ -1,9 +0,0 @@
-import configparser
-
-def read_conf(configFilename):
-    """
-    Dado un nombre de fichero (tipo incluido), carga la configuración y se devuelve mapeada
-    """
-    config = configparser.ConfigParser()
-    config.read(configFilename)
-    return config
