180da651a42433717defab1cfc44fc0a12a11bd9
RafaGB <sernn2@gmail.com>
2020-12-13 18:20:13 +0100

    generado esqueleto de django

--- Aplicaciones/AWS-Manager/awsManager/manage.py
+++ Aplicaciones/AWS-Manager/awsManager/manage.py
@@ -3,21 +3,27 @@
 import os
 import sys
 from dotenv import load_dotenv
-load_dotenv()
+from arq_decorators.arq_decorator import ArqToolsTemplate
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'awsManager.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+class ServerManage(ArqToolsTemplate):
+    def __init__(self, *args, **kwargs):
+        load_dotenv()
+        super().__init__(self.__class__.__name__, *args, **kwargs)
+
+    def run():
+        """Run administrative tasks."""
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'awsManager.settings')
+        try:
+            from django.core.management import execute_from_command_line
+        except ImportError as exc:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            ) from exc
+        execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
-    main()
+    server = ServerManage()
+    server.run()
